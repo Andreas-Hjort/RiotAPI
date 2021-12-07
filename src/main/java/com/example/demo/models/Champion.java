ca0916d5136fd8002f07f0036619e72e234d7fcb
Andreas-Hjort <73465555+Andreas-Hjort@users.noreply.github.com>
2021-12-07 13:37:08 +0100

    Models oprettet og fil struktur lavet

--- src/main/java/com/example/demo/models/Champion.java
+++ src/main/java/com/example/demo/models/Champion.java
@@ -1,4 +1,30 @@
 package com.example.demo.models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.*;
+
+@Data
+@Table(name = "champions")
+@Entity
+@Getter @Setter
 public class Champion {
+
+    @Id
+    @Column
+    private Long championId;
+
+    @Column
+    private String championName;
+
+    @Column
+    private String championTitle;
+
+    @Column
+    private String championDesc;
+
 }
