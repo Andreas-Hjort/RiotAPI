f61adf0a189f888322db8dc4e73f355a5b95d3fb
Andreas-Hjort <73465555+Andreas-Hjort@users.noreply.github.com>
2021-12-07 14:05:47 +0100

    Champions frontend files created

--- src/main/java/com/example/demo/controllers/Champions.java
+++ src/main/java/com/example/demo/controllers/Champions.java
@@ -1,4 +1,21 @@
 package com.example.demo.controllers;
 
+
+
+import com.example.demo.repositories.ChampionsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Champions {
+
+    @Autowired
+    ChampionsRepo championsRepo;
+
+    @GetMapping("/champFront/import")
+    public String (){
+
+
+    }
 }
