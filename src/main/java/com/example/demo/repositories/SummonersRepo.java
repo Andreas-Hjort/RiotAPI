04995c0119df859256b646239f31c75b8774a690
Axel <axelgunde@gmail.com>
2021-12-07 14:26:30 +0100

    Repos

--- src/main/java/com/example/demo/repositories/SummonersRepo.java
+++ src/main/java/com/example/demo/repositories/SummonersRepo.java
@@ -3,5 +3,10 @@ package com.example.demo.repositories;
 import com.example.demo.models.Summoner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SummonersRepo extends JpaRepository<Summoner, Long> {
+public interface SummonersRepo extends JpaRepository<Summoner, String> {
+
+    String url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
+    String apikey = "?api_key=RGAPI-4165caf8-ecd6-46bf-b650-dbeb1b0994c7";
+    String[] usernames = {"Æggemeister3000", "Hjortyy", "Raiden Shogun C2", "Dzukill",
+            "DMONEY1917", "ClintSheeshwood", "Mr Leslie Chow", "thebausffs"};
 }
