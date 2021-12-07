9aaa419bc9189ba8fddb9436052ad66fefc62e26
Axel <axelgunde@gmail.com>
2021-12-07 14:06:17 +0100

    Changed repo interfaces

--- src/main/java/com/example/demo/repositories/ChampionsRepo.java
+++ src/main/java/com/example/demo/repositories/ChampionsRepo.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChampionsRepo extends JpaRepository<Champion,Long> {
-    String url = "https://ddragon.leagueoflegends.com/cdn/11.18.1/data/en_US/champion.json";
-    String apikey = "?api_key=RGAPI-4165caf8-ecd6-46bf-b650-dbeb1b0994c7";
-
-
 
 }
 
--- src/main/java/com/example/demo/repositories/SummonersRepo.java
+++ src/main/java/com/example/demo/repositories/SummonersRepo.java
@@ -3,9 +3,5 @@ package com.example.demo.repositories;
 import com.example.demo.models.Summoner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SummonersRepo extends JpaRepository<Summoner, String> {
-    String url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
-    String apikey = "?api_key=RGAPI-4165caf8-ecd6-46bf-b650-dbeb1b0994c7";
-    String[] summonernames = {"Æggemeister3000","Hjortyy","Raiden Shogun C2","Dzukill",
-            "DMONEY1917","ClintSheeshwood","Mr Leslie Chow", "thebausffs"};
+public interface SummonersRepo extends JpaRepository<Summoner, Long> {
 }
